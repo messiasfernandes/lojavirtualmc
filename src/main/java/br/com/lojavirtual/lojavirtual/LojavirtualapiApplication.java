0413128dfce602e9b62f0d1747a26f24d68ed1c7
Messias Fernandes <messiasfernandes@gmail.com>
2020-08-11 13:15:06 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/lojavirtual/lojavirtual/LojavirtualapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojavirtual.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lojavirtual/lojavirtual/LojavirtualapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojavirtual.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
