425518911db4dae313e742504e837f291b77246c
Messias Fernandes <messiasfernandes@gmail.com>
2020-08-12 10:45:04 -0300

    Teste de inserção no banco e buscar todos registro e buscar por id

--- src/test/java/br/com/lojavirtual/lojavirtual/LojavirtualapiApplicationTests.java
+++ src/test/java/br/com/lojavirtual/lojavirtual/LojavirtualapiApplicationTests.java
@@ -1,17 +1,24 @@
 package br.com.lojavirtual.lojavirtual;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.lojavirtual.lojavirtual.domain.Categoria;
+import br.com.lojavirtual.lojavirtual.rapositorys.CategoriaRepository;
 
 
 
 @SpringBootTest
 class LojavirtualapiApplicationTests {
-
+	@Autowired
+    private CategoriaRepository catdao;
 	@Test
 	void contextLoads() {
-		
+	Categoria cat1 =new Categoria();
+	cat1.setNomecategoria("Perfumes");
+	catdao.save(cat1);
+	System.out.println("Salvo com sucesso");
 	
 	}
 
